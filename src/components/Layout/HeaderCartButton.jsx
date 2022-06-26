bba7513c9a5213dabd37f7333610d3a807d70c7d
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-06-26 13:15:30 +0700

    add bum animation with use Effect

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import Style from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
@@ -6,12 +6,36 @@ import CartContext from "../../store/cart-context";
 function HeaderCartButton({ onClick }) {
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items?.reduce((curNumber, item) => {
+  // destructuring the cart items
+  const { items } = cartCtx;
+
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+  const numberOfCartItems = items?.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${Style.button} ${btnIsHighlighted ? Style.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    // if the cart is empty, remove the highlight
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    // clean up the timer when the component is unmounted
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={Style.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={Style.icon}>
         <CartIcon />
       </span>
